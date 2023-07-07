7bae5c7f7dc0433687d4631fff9d155a020e6515
Jeremy <jeremy.viel@gmail.com>
2023-07-07 13:51:21 +0200

    Put endpoints behind "api" test for testing ingress rule.

--- src/main/java/com/cdn/pru/controller/StockAverageController.java
+++ src/main/java/com/cdn/pru/controller/StockAverageController.java
@@ -4,12 +4,14 @@ import com.cdn.pru.entity.StockAverageInfo;
 import com.cdn.pru.service.StockAverageCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
 @RestController
+@RequestMapping("/api")
 public class StockAverageController {
 
 
